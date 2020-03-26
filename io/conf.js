ad07af145e80f59dbafb4e8c5f66b4e0d020b25a
vicmio <victoire.miossec@ecl19.ec-lyon.fr>
2020-03-26 19:25:28 +0100

    pb node

--- io/conf.js
+++ io/conf.js
@@ -1,6 +1,62 @@
-const socketio = require('socket.io');
+import io from 'socket.io-client';
 
-module.exports = function(server) {
-  // io server
-  const io = socketio(server);
-};
+const socket = io('http://localhost');
+
+
+
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+
+  // game state (players list)
+  const players = {};
+
+  io.sockets.on('connection', function(socket) {
+    // register new player
+    players[socket.id] = {
+      pseudo: "",
+      couleur: "",
+      abs: -1,
+      ord: -1,
+      casePlateau: -1,
+      plateau: [0,0,0,0,0,0,0,0,0],
+    };
+
+    socket.on('nouveau', function(pseudo){
+      socket.pseudo = pseudo;
+      players[socket.id].pseudo = pseudo;
+    });
+
+    socket.on('couleur', function(couleur){
+      players[socket.id].couleur = couleur;
+    });
+
+    socket.on('case',  function(caseCom) {
+      players[socket.id].abs = caseCom[0];
+      players[socket.id].ord = caseCom[1];
+      players[socket.id].casePlateau = caseCom[2];
+      players[socket.id].Plateau[players[socket.id].casePlateau] = 1;
+    });
+
+    // delete disconnected player
+    socket.on('disconnect', function() {
+      delete players[socket.id];
+    });
+  });
+
+
+
+
+
+server.listen(8080);
--- public/game.js
+++ public/game.js
@@ -1,35 +1,28 @@
 
 
+var socket = io.connect('http://localhost:8080');
 
-const clic = [] ;
-var pieceSelectionnee ="";
-
-//definition plateau joueur/ses pions
-
-
-const player = {
-  pseudo: "eriot",
-  couleur: "lion",
-  abs: -1,
-  ord: -1,
-  casePlateau: -1,
-  plateau: [0,0,0,0,0,0,0,0,0],
-};
+let players = [];
 
 var canvas  = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
 
 function drawCoup(){
-  const {couleur, abs, ord, casePlateau, plateau} = player;
-  if (abs != -1 && ord != -1 && casePlateau != -1 && plateau[casePlateau] == 0){
-  var coup = new Image();
-  coup.src = couleur+'.jpeg'; // Image de 80x80 pixels
-  coup.addEventListener('load', function() {
-      context.drawImage(coup, abs, ord);
-    });
-  }
-
+  players.forEach(function({couleur, abs, ord, casePlateau, plateau}) {
+    if (abs != -1 && ord != -1 && casePlateau != -1 && plateau[casePlateau] == 0){
+    var coup = new Image();
+    coup.src = couleur+'.jpeg'; // Image de 80x80 pixels
+    coup.addEventListener('load', function() {
+        context.drawImage(coup, abs, ord);
+      });
+    }
+  });
 }
+
+socket.on('players list', function(list) {
+  players = list;
+});
+
 
 //recup les coordonnees de la souris dans le canva
 function getCoords(el,event) {
@@ -51,62 +44,47 @@ canvas.onclick = function(e) {
 
   if (0 <= x && x< 200){
     if (0<= y && y<200){ //B1
-      player.abs = 0;
-      player.ord = 0;
-      player.casePlateau = 0;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom',[0, 0, 0]);
+
     }
     else if(200<= y && y< 400){ //A1
-      player.abs = 0;
-      player.ord = 200;
-      player.casePlateau = 1;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom',[0, 200, 1]);
+
     }
     else{ //colonne 1
-      player.abs = 0;
-      player.ord = 400;
-      player.casePlateau = 2;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom', [0, 400, 2]);
+
     }
   }
   else if(200 <= x && x< 400){
     if (0<= y && y<200){ //B1
-      player.abs = 200;
-      player.ord = 0;
-      player.casePlateau = 3;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom',[200, 0, 3]);
+
     }
     else if(200<= y && y< 400){ //A1
-      player.abs = 200;
-      player.ord = 200;
-      player.casePlateau = 4;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom',[200, 200, 4]);
+      //player.abs = 200;
+    //  player.ord = 200;
+      //player.casePlateau = 4;
+      //player.Plateau[player.casePlateau] = 1;
     }
     else{ //colonne 1
-      player.abs = 200;
-      player.ord = 400;
-      player.casePlateau = 5;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom', [200, 400, 5]);
+
     }
   }
   else{
     if (0<= y && y<200){ //B1
-      player.abs = 400;
-      player.ord = 0;
-      player.casePlateau = 6;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom',[400, 0, 6]);
+
     }
     else if(200<= y && y< 400){ //A1
-      player.abs = 400;
-      player.ord = 200;
-      player.casePlateau = 7;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom', [400, 200, 7]);
+
     }
     else{ //colonne 1
-      player.abs = 400;
-      player.ord = 400;
-      player.casePlateau = 8;
-      player.Plateau[player.casePlateau] = 1;
+      socket.emit('caseCom',[400, 400, 8]);
+
     }
   }
 
