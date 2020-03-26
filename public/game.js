53d569b811ce87e7ebc515d7c4dd058b65b58ce4
vicmio <victoire.miossec@ecl19.ec-lyon.fr>
2020-03-26 18:21:17 +0100

    on avance

--- public/game.js
+++ public/game.js
@@ -5,24 +5,30 @@ const clic = [] ;
 var pieceSelectionnee ="";
 
 //definition plateau joueur/ses pions
-const plateau = [0,0,0,
-                 0,0,0,
-                 0,0,0];
+
 
 const player = {
-  pseudo = "eriot";
-  couleur = "croix";
+  pseudo: "eriot",
+  couleur: "lion",
+  abs: -1,
+  ord: -1,
+  casePlateau: -1,
+  plateau: [0,0,0,0,0,0,0,0,0],
 };
 
 var canvas  = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
 
-function drawPlateau(joueur, x, y){
+function drawCoup(){
+  const {couleur, abs, ord, casePlateau, plateau} = player;
+  if (abs != -1 && ord != -1 && casePlateau != -1 && plateau[casePlateau] == 0){
   var coup = new Image();
-  coup.src = joueur+'.png'; // Image de 80x80 pixels
+  coup.src = couleur+'.jpeg'; // Image de 80x80 pixels
   coup.addEventListener('load', function() {
-      context.drawImage(coup, x, y);
+      context.drawImage(coup, abs, ord);
     });
+  }
+  
 }
 
 //recup les coordonnees de la souris dans le canva
@@ -40,58 +46,69 @@ function getCoords(el,event) {
 
 canvas.onclick = function(e) {
 
+
   var coords = getCoords(this,e);
-  const {A1, B1, C1, D1, pion1, tourG} = player;
-
-
   x = coords.x;
   y = coords.y;
 
-
-  if (0 <= x && x< 80){
-    if (480<= y && y<560){ //B1
-      context.fillStyle = "yellow";
-      context.beginPath();
-      context.rect(0, 480, 80, 80);
-      context.fill();
-      pieceSelectionnee = B1;
-      player.B1="";
-      player.C2="pion1";
-
+  if (0 <= x && x< 200){
+    if (0<= y && y<200){ //B1
+      player.abs = 0;
+      player.ord = 0;
+      player.casePlateau = 0;
+      player.Plateau[player.casePlateau] = 1;
     }
-    else if(560<= y && y< 640){ //A1
-      context.fillStyle = "green";
-      context.beginPath();
-      context.rect(0,560, 80,80);
-      context.fill();
-      pieceSelectionnee = A1;
+    else if(200<= y && y< 400){ //A1
+      player.abs = 0;
+      player.ord = 200;
+      player.casePlateau = 1;
+      player.Plateau[player.casePlateau] = 1;
     }
     else{ //colonne 1
-      context.fillStyle = "pink";
-      context.beginPath();
-      context.rect(0, y, 80,80);
-      context.fill();
-      pieceSelectionnee = C1;
+      player.abs = 0;
+      player.ord = 400;
+      player.casePlateau = 2;
+      player.Plateau[player.casePlateau] = 1;
     }
   }
-  else if(80 <= x && x< 160){
-    if (480<= y && y<560){
-      context.fillStyle = "yellow";
-      context.beginPath();
-      context.rect(x, y, 80, 80);
-      context.fill();
+  else if(200 <= x && x< 400){
+    if (0<= y && y<200){ //B1
+      player.abs = 200;
+      player.ord = 0;
+      player.casePlateau = 3;
+      player.Plateau[player.casePlateau] = 1;
     }
-    else if(560<= y && y< 640){
-      context.fillStyle = "green";
-      context.beginPath();
-      context.rect(x, y, 80,80);
-      context.fill();
+    else if(200<= y && y< 400){ //A1
+      player.abs = 200;
+      player.ord = 200;
+      player.casePlateau = 4;
+      player.Plateau[player.casePlateau] = 1;
     }
-    else{
-      context.fillStyle = "pink";
-      context.beginPath();
-      context.rect(x, y, 80,80);
-      context.fill();
+    else{ //colonne 1
+      player.abs = 200;
+      player.ord = 400;
+      player.casePlateau = 5;
+      player.Plateau[player.casePlateau] = 1;
+    }
+  }
+  else{
+    if (0<= y && y<200){ //B1
+      player.abs = 400;
+      player.ord = 0;
+      player.casePlateau = 6;
+      player.Plateau[player.casePlateau] = 1;
+    }
+    else if(200<= y && y< 400){ //A1
+      player.abs = 400;
+      player.ord = 200;
+      player.casePlateau = 7;
+      player.Plateau[player.casePlateau] = 1;
+    }
+    else{ //colonne 1
+      player.abs = 400;
+      player.ord = 400;
+      player.casePlateau = 8;
+      player.Plateau[player.casePlateau] = 1;
     }
   }
 
@@ -101,8 +118,8 @@ canvas.onclick = function(e) {
 
 
 function update() {
-  drawPlateau();                                    // 2.
-  //requestAnimationFrame(update);                    // 3.
+  drawCoup();                       // 2.
+  requestAnimationFrame(update);                    // 3.
 }
 
 
