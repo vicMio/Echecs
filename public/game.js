a87048e4ded734b3e620d63ad72788767e8b40fe
vicmio <victoire.miossec@ecl19.ec-lyon.fr>
2020-03-26 17:47:11 +0100

    on passe au morpion

--- public/game.js
+++ public/game.js
@@ -1,222 +1,31 @@
 
 
 
-const coordonnes = {} ;
 const clic = [] ;
+var pieceSelectionnee ="";
 
 //definition plateau joueur/ses pions
+const plateau = [0,0,0,
+                 0,0,0,
+                 0,0,0];
+
 const player = {
-  pseudo: "eriot",
-  couleur: "grey",
-
-  //plateau
-  A1: "tourG",
-  A2: "cavalierG",
-  A3: "fouG",
-  A4: "reine",
-  A5: "roi",
-  A6: "fouD",
-  A7: "cavalierD",
-  A8: "tourD",
-  B1: "pion1",
-  B2: "pion2",
-  B3: "pion3",
-  B4: "pion4",
-  B5: "pion5",
-  B6: "pion6",
-  B7: "pion7",
-  B8: "pion8",
-  C1: "",
-  C2: "",
-  C3: "",
-  C4: "",
-  C5: "",
-  C6: "",
-  C7: "",
-  C8: "",
-  D1: "",
-  D2: "",
-  D3: "",
-  D4: "",
-  D5: "",
-  D6: "",
-  D7: "",
-  D8: "",
-  E1: "",
-  E2: "",
-  E3: "",
-  E4: "",
-  E5: "",
-  E6: "",
-  E7: "",
-  E8: "",
-  F1: "",
-  F2: "",
-  F3: "",
-  F4: "",
-  F5: "",
-  F6: "",
-  F7: "",
-  F8: "",
-  G1: "",
-  G2: "",
-  G3: "",
-  G4: "",
-  G5: "",
-  G6: "",
-  G7: "",
-  G8: "",
-  H1: "",
-  H2: "",
-  H3: "",
-  H4: "",
-  H5: "",
-  H6: "",
-  H7: "",
-  H8: "",
-
-
-
-  pion1: [0, 480],
-  pion2: [80, 480],
-  pion3: [160, 480],
-  pion4: [240, 480],
-  pion5: [320, 480],
-  pion6: [400, 480],
-  pion7: [480, 480],
-  pion8: [560, 480],
-
-  tourG: [0, 560],
-  cavalierG: [80, 560],
-  fouG: [160, 560],
-  reine: [240, 560],
-  roi: [320, 560],
-  fouD: [400, 560],
-  cavalierD: [480, 560],
-  tourD: [560, 560],
-
-
+  pseudo = "eriot";
+  couleur = "croix";
 };
 
 var canvas  = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
 
-function drawPlayers() {
-  const {pion1, pion2, pion3, pion4, pion5, pion6, pion7, pion8, tourG, cavalierG, fouG, reine, roi, fouD, cavalierD, tourD, couleur} = player;
-  context.fillStyle = couleur;
-
-  var pion1 = new Image();
-  pion1.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion1.addEventListener('load', function() {
-      context.drawImage(pion1, pion1[0], pion1[1]);
-      coordonnes["pion1"] = [pion1[0], pion1[1]];
+function drawPlateau(joueur, x, y){
+  var coup = new Image();
+  coup.src = joueur+'.png'; // Image de 80x80 pixels
+  coup.addEventListener('load', function() {
+      context.drawImage(coup, x, y);
     });
-
-  var pion2 = new Image();
-  pion2.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion2.addEventListener('load', function() {
-      context.drawImage(pion2, pion2[0], pion2[1]);
-      coordonnes["pion2"] = [pion2[0], pion2[1]];
-    });
-
-  var pion3 = new Image();
-  pion3.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion3.addEventListener('load', function() {
-      context.drawImage(pion3, pion3[0], pion3[1]);
-      coordonnes["pion3"] = [pion3[0], pion3[1]];
-    });
-
-  var pion4 = new Image();
-  pion4.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion4.addEventListener('load', function() {
-      context.drawImage(pion4, pion4[0], pion4[1]);
-      coordonnes["pion4"] = [pion4[0], pion4[1]];
-    });
-
-  var pion5 = new Image();
-  pion5.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion5.addEventListener('load', function() {
-      context.drawImage(pion5, pion5[0], pion5[1]);
-      coordonnes["pion5"] = [pion5[0], pion5[1]];
-    });
-
-  var pion6 = new Image();
-  pion6.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion6.addEventListener('load', function() {
-      context.drawImage(pion6, pion6[0], pion6[1]);
-      coordonnes["pion6"] = [pion6[0], pion6[1]];
-  });
-
-  var pion7 = new Image();
-  pion7.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion7.addEventListener('load', function() {
-      context.drawImage(pion7, pion7[0], pion7[1]);
-      coordonnes["pion7"] = [pion7[0], pion7[1]];
-  });
-
-  var pion8 = new Image();
-  pion8.src = 'pionNoir.png'; // Image de 80x80 pixels
-  pion8.addEventListener('load', function() {
-      context.drawImage(pion8, pion8[0], pion8[1]);
-      coordonnes["pion8"] = [pion8[0], pion8[1]];
-  });
-
-  var tourG = new Image();
-  tourG.src = 'tourNoir.png'; // Image de 80x80 pixels
-  tourG.addEventListener('load', function() {
-  context.drawImage(tourG, tourG[0], tourG[1]);
-  });
-
-  var tourD = new Image();
-  tourD.src = 'tourNoir.png'; // Image de 80x80 pixels
-  tourD.addEventListener('load', function() {
-  context.drawImage(tourD, tourD[0], tourD[1]);
-  });
-
-  var cavalierG = new Image();
-  cavalierG.src = 'cavalierNoir.png'; // Image de 80x80 pixels
-  cavalierG.addEventListener('load', function() {
-  context.drawImage(cavalierG, cavalierG[0], cavalierG[1]);
-  });
-
-  var cavalierD = new Image();
-  cavalierD.src = 'cavalierNoir.png'; // Image de 80x80 pixels
-  cavalierD.addEventListener('load', function() {
-  context.drawImage(cavalierD, cavalierD[0], cavalierD[1]);
-  });
-
-  var fouG = new Image();
-  fouG.src = 'fouNoir.png'; // Image de 80x80 pixels
-  fouG.addEventListener('load', function() {
-  context.drawImage(fouG, fouG[0], fouG[1]);
-  });
-
-  var fouD = new Image();
-  fouD.src = 'fouNoir.png'; // Image de 80x80 pixels
-  fouD.addEventListener('load', function() {
-  context.drawImage(fouD, fouD[0], fouD[1]);
-  });
-
-  var reine = new Image();
-  reine.src = 'reineNoir.png'; // Image de 80x80 pixels
-  reine.addEventListener('load', function() {
-  context.drawImage(reine, reine[0], reine[1]);
-  });
-
-  var roi = new Image();
-  roi.src = 'roiNoir.png'; // Image de 80x80 pixels
-  roi.addEventListener('load', function() {
-  context.drawImage(roi, roi[0], roi[1]);
-  });
-
 }
 
-function update() {
-  drawPlayers();                                    // 2.
-  requestAnimationFrame(update);                    // 3.
-}
-
-
+//recup les coordonnees de la souris dans le canva
 function getCoords(el,event) {
   var ox = el.scrollLeft - el.offsetLeft,
   oy = el.scrollTop - el.offsetTop;
@@ -228,18 +37,16 @@ function getCoords(el,event) {
 }
 
 
-// Exemple d'utilisation :
 
-var canvas  = document.querySelector('#canvas');
 canvas.onclick = function(e) {
 
-  var context = canvas.getContext('2d');
   var coords = getCoords(this,e);
-  const {A1, A2, A3, B1, C1, D1} = player;
-  var piece ="";
+  const {A1, B1, C1, D1, pion1, tourG} = player;
+
 
   x = coords.x;
   y = coords.y;
+
 
   if (0 <= x && x< 80){
     if (480<= y && y<560){ //B1
@@ -247,9 +54,9 @@ canvas.onclick = function(e) {
       context.beginPath();
       context.rect(0, 480, 80, 80);
       context.fill();
-      if (A1 != ""){
-
-      }
+      pieceSelectionnee = B1;
+      player.B1="";
+      player.C2="pion1";
 
     }
     else if(560<= y && y< 640){ //A1
@@ -257,12 +64,14 @@ canvas.onclick = function(e) {
       context.beginPath();
       context.rect(0,560, 80,80);
       context.fill();
+      pieceSelectionnee = A1;
     }
     else{ //colonne 1
       context.fillStyle = "pink";
       context.beginPath();
       context.rect(0, y, 80,80);
       context.fill();
+      pieceSelectionnee = C1;
     }
   }
   else if(80 <= x && x< 160){
@@ -285,91 +94,16 @@ canvas.onclick = function(e) {
       context.fill();
     }
   }
-  else if(160 <= x && x< 240){
-    if (480<= y <560){
-
-    }
-    else if(560<= y < 640){
-
-    }
-    else{
-
-    }
-  }
-  else if(240 <= x && x< 320){
-    if (480<= y <560){
-
-    }
-    else if(560<= y < 640){
-
-    }
-    else{
-
-    }
-  }
-  else if(320 <= x && x< 400){
-    if (480<= y <560){
-
-    }
-    else if(560<= y < 640){
-
-    }
-    else{
-
-    }
-  }
-  else if(400 <= x && x< 480){
-    if (480<= y <560){
-
-    }
-    else if(560<= y < 640){
-
-    }
-    else{
-
-    }
-  }
-  else if(480 <= x && x< 560){
-    if (480<= y <560){
-
-    }
-    else if(560<= y < 640){
-
-    }
-    else{
-
-    }
-  }
-  else if(560 <= x && x< 640){
-    if (480<= y <560){
-
-    }
-    else if(560<= y < 640){
-
-    }
-    else{
-
-    }
-  }
-  else{
-    if (480<= y <560){
-
-    }
-    else if(560<= y < 640){
-
-    }
-    else{
-
-    }
-  }
-
 
 };
 
 
 
 
-
+function update() {
+  drawPlateau();                                    // 2.
+  //requestAnimationFrame(update);                    // 3.
+}
 
 
 function moveElement() {
@@ -383,11 +117,122 @@ function moveElement() {
 
 
 
-
-
-
-
-
-
 // first call
 requestAnimationFrame(update);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////// SI BESOIN
+function drawPlayers() {
+  const {pion1, pion2, pion3, pion4, pion5, pion6, pion7, pion8, tourG, cavalierG, fouG, reine, roi, fouD, cavalierD, tourD, couleur} = player;
+  context.fillStyle = couleur;
+
+  var pion1Var = new Image();
+  pion1Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion1Var.addEventListener('load', function() {
+      context.drawImage(pion1Var, pion1[0], pion1[1]);
+    });
+
+  var pion2Var = new Image();
+  pion2Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion2Var.addEventListener('load', function() {
+      context.drawImage(pion2Var, pion2[0], pion2[1]);
+    });
+
+  var pion3Var = new Image();
+  pion3Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion3Var.addEventListener('load', function() {
+      context.drawImage(pion3Var, pion3[0], pion3[1]);
+    });
+
+  var pion4Var = new Image();
+  pion4Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion4Var.addEventListener('load', function() {
+      context.drawImage(pion4Var, pion4[0], pion4[1]);
+    });
+
+  var pion5Var = new Image();
+  pion5Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion5Var.addEventListener('load', function() {
+      context.drawImage(pion5Var, pion5[0], pion5[1]);
+    });
+
+  var pion6Var = new Image();
+  pion6Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion6Var.addEventListener('load', function() {
+      context.drawImage(pion6Var, pion6[0], pion6[1]);
+  });
+
+  var pion7Var = new Image();
+  pion7Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion7Var.addEventListener('load', function() {
+      context.drawImage(pion7Var, pion7[0], pion7[1]);
+  });
+
+  var pion8Var = new Image();
+  pion8Var.src = 'pionNoir.png'; // Image de 80x80 pixels
+  pion8Var.addEventListener('load', function() {
+      context.drawImage(pion8Var, pion8[0], pion8[1]);
+  });
+
+  var tourGVar = new Image();
+  tourGVar.src = 'tourNoir.png'; // Image de 80x80 pixels
+  tourGVar.addEventListener('load', function() {
+  context.drawImage(tourGVar, tourG[0], tourG[1]);
+  });
+
+  var tourDVar = new Image();
+  tourDVar.src = 'tourNoir.png'; // Image de 80x80 pixels
+  tourDVar.addEventListener('load', function() {
+  context.drawImage(tourDVar, tourD[0], tourD[1]);
+  });
+
+  var cavalierGVar = new Image();
+  cavalierGVar.src = 'cavalierNoir.png'; // Image de 80x80 pixels
+  cavalierGVar.addEventListener('load', function() {
+  context.drawImage(cavalierGVar, cavalierG[0], cavalierG[1]);
+  });
+
+  var cavalierDVar = new Image();
+  cavalierDVar.src = 'cavalierNoir.png'; // Image de 80x80 pixels
+  cavalierDVar.addEventListener('load', function() {
+  context.drawImage(cavalierDVar, cavalierD[0], cavalierD[1]);
+  });
+
+  var fouGVar = new Image();
+  fouGVar.src = 'fouNoir.png'; // Image de 80x80 pixels
+  fouGVar.addEventListener('load', function() {
+  context.drawImage(fouGVar, fouG[0], fouG[1]);
+  });
+
+  var fouDVar = new Image();
+  fouDVar.src = 'fouNoir.png'; // Image de 80x80 pixels
+  fouDVar.addEventListener('load', function() {
+  context.drawImage(fouDVar, fouD[0], fouD[1]);
+  });
+
+  var reineVar = new Image();
+  reineVar.src = 'reineNoir.png'; // Image de 80x80 pixels
+  reineVar.addEventListener('load', function() {
+  context.drawImage(reineVar, reine[0], reine[1]);
+  });
+
+  var roiVar = new Image();
+  roiVar.src = 'roiNoir.png'; // Image de 80x80 pixels
+  roiVar.addEventListener('load', function() {
+  context.drawImage(roiVar, roi[0], roi[1]);
+  });
+
+}
--- public/plateau.js
+++ public/plateau.js
@@ -2,42 +2,11 @@ var canvas  = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
 context.fillStyle = "red";
 
-context.fillRect(0, 0, 80, 80);
-context.fillRect(160, 0, 80, 80);
-context.fillRect(320, 0, 80, 80);
-context.fillRect(480, 0, 80, 80);
+context.fillRect(0, 0, 200, 200);
+context.fillRect(400, 0, 200, 200);
 
-context.fillRect(0, 160, 80, 80);
-context.fillRect(160, 160, 80, 80);
-context.fillRect(320, 160, 80, 80);
-context.fillRect(480, 160, 80, 80);
 
-context.fillRect(0, 320, 80, 80);
-context.fillRect(160, 320, 80, 80);
-context.fillRect(320, 320, 80, 80);
-context.fillRect(480, 320, 80, 80);
+context.fillRect(200, 200, 200, 200);
 
-context.fillRect(0, 480, 80, 80);
-context.fillRect(160, 480, 80, 80);
-context.fillRect(320, 480, 80, 80);
-context.fillRect(480, 480, 80, 80);
-
-context.fillRect(80,80 , 80, 80);
-context.fillRect(240,80 , 80, 80);
-context.fillRect(400,80 , 80, 80);
-context.fillRect(560,80 , 80, 80);
-
-context.fillRect(80,240 , 80, 80);
-context.fillRect(240,240 , 80, 80);
-context.fillRect(400,240 , 80, 80);
-context.fillRect(560,240 , 80, 80);
-
-context.fillRect(80,400 , 80, 80);
-context.fillRect(240,400 , 80, 80);
-context.fillRect(400,400 , 80, 80);
-context.fillRect(560,400 , 80, 80);
-
-context.fillRect(80,560 , 80, 80);
-context.fillRect(240,560 , 80, 80);
-context.fillRect(400,560 , 80, 80);
-context.fillRect(560,560 , 80, 80);
+context.fillRect(0, 400, 200, 200);
+context.fillRect(400, 400, 200, 200);
